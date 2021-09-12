9fa47b382ff43995e80c65ab717aa07140b82b78
Mika Anttila <mmoa@iki.fi>
2021-09-12 13:48:31 +0000

    Merge new styles from testi.html

--- new.php
+++ new.php
@@ -7,10 +7,16 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 <html lang="fi">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <?php
 echo "<title>Ilmo " . $tapahtuma . "</title>\n";
 ?>
-<style> .short-width td {   width: 10px; } </style>
+<style> 
+.nro { width: 5; } 
+.nimi { width: 5; text-align: left; } 
+.center { border: 1px solid black; text-align: center; }
+.separator { border: 1px solid black; text-align: center; background: #b6b6b6; }
+</style>
 </head>
 <body>
 <?php
@@ -37,16 +43,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 echo "<H2>" . $tapahtuma . " ilmoittautuminen</H2>\n";
 ?>
-<table border="1" style="width:50%">
-<!-- <col style="width:10%"/><col style="width:90%"/> -->
-<thead><tr><th style="width:1px">Nro:</th><th>Nimi:</th></tr></thead>
-<tbody>
 <form method="POST">
-<input type="hidden" name="Tapahtuma" value="">
-<tr><td style="width:1px"><input type="number" name="Nro" size="4" value=""></td><td><input type="text" name="Nimi" value""></td></tr>
-<tr><td colspan="2" align="center"><input type="Submit" value="Lisää"></td></tr>
+<table style="border:1px">
+<col style="width:10%"/><col style="width:90%"/> 
+<thead><tr><th class="nro">Nro:</th><th class="nimi">Nimi:</th></tr></thead>
+<tbody>
+<tr><td style="width:1px"><input type="number" name="Nro" max="9999" value=""></td><td><input type="text" name="Nimi" size="64" value""></td></tr>
+<tr><td colspan="2" style="width:30%"><input type="Submit" value="Lisää"></td></tr>
+</tbody></table>
+<div class="separator">Ilmoittautuneet:</div>
 <tr><td colspan="2" bgcolor="#b6b6b6" align="center">Ilmoittautuneet:</td></tr>
 </form>
+<table>
 <tr>
 <?php
 $sorted = read_csv($file);
