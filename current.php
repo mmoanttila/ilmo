e550c8552a2edb126a5081f1c01c04da527d0d24
Mika Anttila <mmoa-github@mmoa.net>
2021-10-13 19:22:03 +0300

    Lets try to read json

--- current.php
+++ current.php
@@ -2,5 +2,8 @@
 $tapahtuma = "Järilänvuoren sprintti";
 $pvm = "2021-09-18";
 $file = "ilmot/jarilan-sprint.csv";
-
+$json = json_decode(file_get_contents("current.json"));
+$tapahtuma = $json['tapahtuma'];
+$pvm = $json['pvm'];
+$file = $json['file'];
 ?>
