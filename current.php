f1b3d2c4c7a7908cd4bd46068878311dce7d3000
Mika Anttila <mmoa@iki.fi>
2021-10-13 19:29:52 +0300

    We need to read as associative array

--- current.php
+++ current.php
@@ -2,7 +2,7 @@
 $tapahtuma = "Järilänvuoren sprintti";
 $pvm = "2021-09-18";
 $file = "ilmot/jarilan-sprint.csv";
-$json = json_decode(file_get_contents("current.json"));
+$json = json_decode(file_get_contents("current.json"), true);
 $tapahtuma = $json['tapahtuma'];
 $pvm = $json['pvm'];
 $file = $json['file'];
