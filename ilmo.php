8f3f810f1877d4ba6416d32bdd7cdc324806abbc
Mika Anttila <mika.anttila@csc.fi>
2025-11-07 18:42:03 +0200

    First version of vastuuvapaus

--- ilmo.php
+++ ilmo.php
@@ -11,11 +11,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <body>
 <h2>Ilmoittautuminen</h2>
+<?php
+if ($_POST["TT"] != "true" { ?>
+<pre><?php include("vastuuvapaus.txt") ?></pre>
+<form method="POST">
+    <input type="check" name="TT" value="true">
+    <input type="hidden" name="Nro" value="<?php $nro ?>">
+    <input type="hidden" name="Nimi" value="<?php $nimi ?>">
+    <input type="hidden" name="Tapahtuma" value="<?php $tapahtuma ?>">
+    <input type="submit"  name="Hyväksy" value="Hyväksy">
+</form>
+<?php
+} else { ?>
 <p> Otin vastaan ilmoittautumisen:
 <table>
   <tr><td># <?php echo $nro; ?></td><td>Nimi: <?php echo $_POST["Nimi"]; ?></td></tr>
 </table>
-
 
 <?php
 $added=addcsv ( $nro, $nimi, $tapahtuma . ".csv" );
@@ -24,6 +35,7 @@ if ( $added == FALSE ) {
 <pre>Tiedoston kirjoitus ei onnistunut!!</pre>
 <?php
 } else {
+}
 }
 ?>
 </body>
