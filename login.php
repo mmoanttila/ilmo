1773402a21a95a051948ebf7ae42b6a7831dd2e5
Mika Anttila <mika.anttila@csc.fi>
2026-01-24 19:33:38 +0200

    Fix admin login for mobile devices

--- login.php
+++ login.php
@@ -17,6 +17,8 @@ if(count($_POST) > 0)
 <!DOCTYPE html>
 <html lang="fi">
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
    <title>Ilmo<TM></title>
 </head>
 <body>
